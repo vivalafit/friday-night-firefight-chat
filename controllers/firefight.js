dce5771cf38f28960cae28c8d258efd97b015e2f
Max Navrotskyi <m.navrotskii@pnn.in.ua>
2020-11-18 00:06:00 +0200

    STUN/ DEATH saves update

--- controllers/firefight.js
+++ controllers/firefight.js
@@ -1,5 +1,6 @@
-const e = require('express');
-var Roll = require('roll');
+const Roll = require('roll');
+const srand = require('srand');
+srand.seed(Date.now());
 const serverCache = require('../utils/server-cache');
 const HIT_LOCATIONS = {
     1 : "head",
@@ -25,6 +26,18 @@ const WOUND_LEVEL_MOD = {
     9: -8,
     10: -9
 }
+const WOUND_LEVEL_STR = {
+    "0": "LIGHT",
+    "-1" : "SERIOUS",
+    "-2" : "CRITICAL",
+    "-3" : "MORTAL 0",
+    "-4" : "MORTAL 1",
+    "-5" : "MORTAL 2",
+    "-6" : "MORTAL 3",
+    "-7" : "MORTAL 4",
+    "-8" : "MORTAL 5",
+    "-9" : "MORTAL 6"
+}
 const MAX_WOUND_LEVEL = 40;
 const WOUND_LEVEL_BOUND = 4;
 const MAXIMUM_STUN_LEVEL = 3;
@@ -38,7 +51,6 @@ exports.countBattle = async (data) => {
         }
         let roomCache = serverCache.get(data.roomId);
         let logStr = "";
-        const roller = new Roll();
         //test cache - BEGIN
         roomCache = {
             goons: [{
@@ -93,7 +105,7 @@ exports.countBattle = async (data) => {
                     },
                     fightStats: {
                         ref: 5,
-                        body: 8,
+                        body: 6,
                         btm: 3,
                         wpn: 3,
                         mods: "0"
@@ -117,7 +129,7 @@ exports.countBattle = async (data) => {
         let targetObj = roomCache[targetType][targetArr[1]];
         // 1) SINGLE FIRE MOD
         for(let i = 0; i < battleData.wpnBullets; i++) {
-            const shotCalculations = calculateShotDmg(logStr, shooterObj, targetObj, shooterAimMods, battleData, i, roller);
+            const shotCalculations = calculateShotDmg(logStr, shooterObj, targetObj, shooterAimMods, battleData, i);
             if(shotCalculations.break === true){
                 logStr =  shotCalculations.logStr;
                 break;
@@ -128,6 +140,15 @@ exports.countBattle = async (data) => {
             }
         }
         // 1) SINGLE FIRE MOD - END
+        // Check if target is stunned or dead
+        let stunDeathSummary = "";
+       if(targetObj.stunned) {
+            stunDeathSummary = `<div class="title-summary stunned">Target shocked and fainted on ${targetObj.stunnedOn} shot!</div>`;
+        }
+        if(targetObj.dead) {
+            stunDeathSummary = `${stunDeathSummary}<div class="title-summary dead">Target is dead on ${targetObj.deadOn} shot!</div>`;
+        } 
+        logStr = `${stunDeathSummary}${logStr}`
         data.io.to(data.roomId).emit('calculation-completed', {logStr: logStr});
     } catch (e) {
         data.io.to(data.roomId).emit('calculation-completed', {logStr: `<div class="shot-landed armor-penetration">Error ${e}.</div>`});
@@ -140,42 +161,79 @@ exports.countBattle = async (data) => {
 }
 
 //uses default rules for Cyberpunk 2020 stun/death saves - modify constants value whatever you want to.
-const calculateStunDeathSave = (logStr, targetObj, hitLocation, bulletDmg) => {
-    //
+const calculateStunDeathSave = (logStr, targetObj, hitLocation, bulletDmg, shotNumber) => {
     targetObj.woundLevel = targetObj.woundLevel + bulletDmg >= MAX_WOUND_LEVEL ? MAX_WOUND_LEVEL : targetObj.woundLevel + bulletDmg;
+    const woundLevel = Math.ceil(targetObj.woundLevel / WOUND_LEVEL_BOUND);
+    const woundLevelMod = WOUND_LEVEL_MOD[woundLevel];
+    const bodyWithPenalty = targetObj.fightStats.body + woundLevelMod;
+    const roller = new Roll(function () {
+        return srand.random();
+    });
+   
     // DEATH ROLL - higher than 3, beginning from MORTAL 0
-    if(Math.ceil(targetObj.woundLevel / WOUND_LEVEL_BOUND) > MAXIMUM_STUN_LEVEL) {
-        const a = 1;
+    if(Math.ceil(targetObj.woundLevel / WOUND_LEVEL_BOUND) > MAXIMUM_STUN_LEVEL && !targetObj.dead) {
+        logStr = `${logStr}<div class="stun-save death-stun-title">Target has to roll ${bodyWithPenalty} or lower to pass the DEATH save!</div>`
+        logStr = `${logStr}<div class="stun-save hint">(Target's BODY: ${targetObj.fightStats.body}, wound level MOD: ${WOUND_LEVEL_STR[woundLevelMod]}/${woundLevelMod})</div>`
+        const rollResult = roller.roll("1d10").result;
+        if(rollResult > bodyWithPenalty){
+            logStr = `${logStr}<div class="death-save">It is seems like Target <span class="failed-roll">failed</span> the roll <span class="shot-value">${rollResult}</span> vs <span class="shot-value">${bodyWithPenalty}</span>!</div>`
+            logStr = `${logStr}<div class="death-save failed-roll-span">Target is dead!</div>`
+            //mutate object with custom flag - to avoid later stun saves 
+            targetObj.dead = true;
+            targetObj.deadOn = shotNumber + 1;
+        } else {
+            logStr = `${logStr}<div class="death-save">It is seems like Target <span class="passed-roll">passed</span> the roll <span class="shot-value">${rollResult}</span> vs <span class="shot-value">${bodyWithPenalty}</span>!</div>`
+            logStr = `${logStr}<div class="death-save passed-roll-span">Target is quite alive for this moment!</div>`
+        }
     } 
     // If wound level is lower than boundaries of SERIOUS wound (3) - STUN Roll
-    else {
-        const a = 1;
+    else if(!targetObj.stunned && !targetObj.dead) {
+        logStr = `${logStr}<div class="stun-save death-stun-title">Target has to roll ${bodyWithPenalty} or lower to pass the STUN save!</div>`
+        logStr = `${logStr}<div class="stun-save hint">(Target's BODY: ${targetObj.fightStats.body}, wound level MOD: ${WOUND_LEVEL_STR[woundLevelMod]}/${woundLevelMod})</div>`
+        const rollResult = roller.roll("1d10").result;
+        if(rollResult > bodyWithPenalty){
+            logStr = `${logStr}<div class="stun-save">It is seems like Target <span class="failed-roll">failed</span> the roll <span class="shot-value">${rollResult}</span> vs <span class="shot-value">${bodyWithPenalty}</span>!</div>`
+            logStr = `${logStr}<div class="stun-save failed-roll-span">Target shocked and fainted!</div>`
+            //mutate object with custom flag - to avoid later stun saves 
+            targetObj.stunned = true;
+            targetObj.stunnedOn = shotNumber + 1;
+        } else {
+            logStr = `${logStr}<div class="stun-save">It is seems like Target <span class="passed-roll">passed</span> the roll <span class="shot-value">${rollResult}</span> vs <span class="shot-value">${bodyWithPenalty}</span>!</div>`
+            logStr = `${logStr}<div class="stun-save passed-roll-span">Target is still conscious!</div>`
+        }
+    }
+    return {
+        logStr: logStr,
+        targetObj: targetObj
     }
 }
 
-const calculateArmorDmg = (logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj) => {
+const calculateArmorDmg = (logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj, shotNumber) => {
   //check if bullet penetrated target`s armor
-  if(bulletDmg > targetLocationArmor){
-    //reduce target`s armor if bullet penetrated it + reduce bullet damage with armor`s value
-    logStr = `${logStr}<div class="shot-landed armor-penetration">Target's Armor value(<span class="shot-value">${targetLocationArmor}</span>) reduced bullet damage from <span class="shot-value">${bulletDmg}</span> -> <span class="shot-value">${bulletDmg - targetLocationArmor}</span>.</div>`
-    bulletDmg = bulletDmg - targetLocationArmor;
-    targetObj.bodyStats.armor[hitLocation] = targetLocationArmor - 1 >= 0 ? targetLocationArmor - 1 : 0;
-    logStr = `${logStr}<div class="shot-landed armor-penetration">Shot with damage <span class="shot-value">${bulletDmg}</span> penetrated the target's armor on <span class="shot-part-info">${hitLocation}</span>.</div>`
+    if(bulletDmg > targetLocationArmor){
+        //reduce target`s armor if bullet penetrated it + reduce bullet damage with armor`s value
+        logStr = `${logStr}<div class="shot-landed armor-penetration">Target's Armor value(<span class="shot-value">${targetLocationArmor}</span>) reduced bullet damage from <span class="shot-value">${bulletDmg}</span> -> <span class="shot-value">${bulletDmg - targetLocationArmor}</span>.</div>`
+        bulletDmg = bulletDmg - targetLocationArmor;
+        targetObj.bodyStats.armor[hitLocation] = targetLocationArmor - 1 >= 0 ? targetLocationArmor - 1 : 0;
+        logStr = `${logStr}<div class="shot-landed armor-penetration">Shot with damage <span class="shot-value">${bulletDmg}</span> penetrated the target's armor on <span class="shot-part-info">${hitLocation}</span>.</div>`
 
-    //reduced damage - applied btm value to it
-    let BTMedDamage = bulletDmg - targetObj.fightStats.btm <= 0 ? 1 : bulletDmg - targetObj.fightStats.btm;
-    logStr = `${logStr}<div class="shot-landed">Target's BTM value(<span class="shot-value">${targetObj.fightStats.btm}</span>) reduced bullet damage <span class="shot-value">${bulletDmg}</span> -> <span class="shot-value">${BTMedDamage}</span>.</div>`
-    if(hitLocation === "head") {
-        BTMedDamage = BTMedDamage * 2;
-        logStr = `${logStr}<div class="shot-landed">Bullet has hit the head so the damage is doubled(<span class="shot-value">${BTMedDamage}</span>)!</div>`
+        //reduced damage - applied btm value to it
+        let BTMedDamage = bulletDmg - targetObj.fightStats.btm <= 0 ? 1 : bulletDmg - targetObj.fightStats.btm;
+        logStr = `${logStr}<div class="shot-landed">Target's BTM value(<span class="shot-value">${targetObj.fightStats.btm}</span>) reduced bullet damage <span class="shot-value">${bulletDmg}</span> -> <span class="shot-value">${BTMedDamage}</span>.</div>`
+        if(hitLocation === "head") {
+            BTMedDamage = BTMedDamage * 2;
+            logStr = `${logStr}<div class="shot-landed">Bullet has hit the head so the damage is doubled(<span class="shot-value">${BTMedDamage}</span>)!</div>`
 
-    }
-    let targetLocationHP = targetObj.bodyStats.limbs[hitLocation] - BTMedDamage < 0 ? 0 : targetObj.bodyStats.limbs[hitLocation] - BTMedDamage;
-    logStr = `${logStr}<div class="shot-landed armor-penetration armor-left">Target's armor left on ${hitLocation} : <span class="shot-value">${targetObj.bodyStats.armor[hitLocation]}</span>.</div>`
-    logStr = `${logStr}<div class="shot-landed hp-left">Target's health left on ${hitLocation} : <span class="shot-value">${targetLocationHP}</span>.</div>`
-    //update limb HP value
-    targetObj.bodyStats.limbs[hitLocation] = targetLocationHP;
-    const stunDeathSaveResult = calculateStunDeathSave(logStr, targetObj, hitLocation, BTMedDamage);
+        }
+        let targetLocationHP = targetObj.bodyStats.limbs[hitLocation] - BTMedDamage < 0 ? 0 : targetObj.bodyStats.limbs[hitLocation] - BTMedDamage;
+        logStr = `${logStr}<div class="shot-landed armor-penetration armor-left">Target's armor left on ${hitLocation} : <span class="shot-value">${targetObj.bodyStats.armor[hitLocation]}</span>.</div>`
+        logStr = `${logStr}<div class="shot-landed hp-left">Target's health left on ${hitLocation} : <span class="shot-value">${targetLocationHP}</span>.</div>`
+        //update limb HP value
+        targetObj.bodyStats.limbs[hitLocation] = targetLocationHP;
+        //calculate STUN/DEATH save
+        const stunDeathSaveResult = calculateStunDeathSave(logStr, targetObj, hitLocation, BTMedDamage, shotNumber);
+        logStr = stunDeathSaveResult.logStr;
+        targetObj = stunDeathSaveResult.targetObj;
     } else {
         //shot does not penetrated armor
         logStr = `${logStr}<div class="shot-landed not-armor-penetration">Bullet with damage(<span class="shot-value">${bulletDmg}</span>) does not penetrated armor(<span class="shot-value">${targetLocationArmor}</span>) on <span class="shot-part-info">${hitLocation}</span>.</div>`
@@ -194,7 +252,7 @@ const calculateCoverArmorDmg = (logStr, bulletDmg, targetLocationArmor, hitLocat
         battleData.coverValue = battleData.coverValue - 1 >= 0 ? battleData.coverValue -1 : 0;
         logStr = `${logStr}<div class="shot-landed armor-penetration">Shot with damage <span class="shot-value">${bulletDmg}</span> penetrated the cover armor!</div>`
         //armor calculations
-        const armorCalculationResult = calculateArmorDmg(logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj);
+        const armorCalculationResult = calculateArmorDmg(logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj, shotNumber);
         logStr = armorCalculationResult.logStr;
         targetObj = armorCalculationResult.targetObj;
         logStr = `${logStr}<div class="shot-landed armor-penetration cover-left">Updated Cover Value: ${battleData.coverValue}</div>`
@@ -209,8 +267,12 @@ const calculateCoverArmorDmg = (logStr, bulletDmg, targetLocationArmor, hitLocat
     }
 }
 
-const calculateShotDmg = (logStr, shooterObj, targetObj, shooterAimMods, battleData, shotNumber, roller) => {
+const calculateShotDmg = (logStr, shooterObj, targetObj, shooterAimMods, battleData, shotNumber) => {
     //roll if bullet hit target
+    //generate random seed for every shot - to ensure that every shot at least uses random seed for pseudo-random roll gen
+    const roller = new Roll(function () {
+        return srand.random();
+    });
     const rollResult = roller.roll("1d10").result;
     // CRITICAL FAILURE : WEAPON FUMBLED - END BULLET STORM THEN
     if(rollResult === 1){
@@ -257,12 +319,12 @@ const calculateShotDmg = (logStr, shooterObj, targetObj, shooterAimMods, battleD
         let targetLocationArmor = targetObj.bodyStats.armor[hitLocation];
         if(battleData.coverValue || battleData.coverValue > 0) {
             //cover armor calculations
-            const coverCalculations = calculateCoverArmorDmg(logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj, battleData);
+            const coverCalculations = calculateCoverArmorDmg(logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj, battleData, shotNumber);
             logStr = coverCalculations.logStr;
             targetObj = coverCalculations.targetObj;
         } else {
             //armor calculations
-            const armorCalculationsResult = calculateArmorDmg(logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj);
+            const armorCalculationsResult = calculateArmorDmg(logStr, bulletDmg, targetLocationArmor, hitLocation, targetObj, shotNumber);
             logStr = armorCalculationsResult.logStr;
             targetObj = armorCalculationsResult.targetObj;
         }
