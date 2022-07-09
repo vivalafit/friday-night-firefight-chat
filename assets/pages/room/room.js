b94ecdd2fd0c947e1adb319fe8fb176b4ae058db
Max Navrotskyi <navrotskymx@gmail.com>
2022-07-09 14:46:41 +0300

    toast stylings update

--- assets/pages/room/room.js
+++ assets/pages/room/room.js
@@ -349,9 +349,10 @@ const donwloadSucceeded = () => {
     allowToastClose: true, // Boolean value true or false
     hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
     stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
-    position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
-    
-    
+    position: {
+      right: 40,
+      bottom: 35
+    }, // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
     
     textAlign: 'left',  // Text alignment i.e. left, right or center
     loader: true,  // Whether to show loader or not. True by default
@@ -372,7 +373,10 @@ const downloadFailed = () => {
     allowToastClose: true, // Boolean value true or false
     hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
     stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
-    position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+    position: {
+      right: 40,
+      bottom: 35
+    }, // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
     
     
     
