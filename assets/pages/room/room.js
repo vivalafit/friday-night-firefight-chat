efc46dd3818c2261b976a871c0bae1f010860b3e
Max Navrotskyi <navrotskymx@gmail.com>
2022-07-09 14:19:38 +0300

    feature: added toast library for notifications

--- assets/pages/room/room.js
+++ assets/pages/room/room.js
@@ -318,8 +318,10 @@ const initHandlers = (socket) => {
       url: "/" + ROOM_ID + "/export-goons",
       success: function (data) {
         download(data);
+        donwloadSucceeded();
       },
       error: function (e) {
+        downloadFailed();
         console.log(e);
       }
     });
@@ -336,6 +338,52 @@ const download = (data) => {
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
+}
+
+const donwloadSucceeded = () => {
+  $.toast({
+    text: "Successfully Downloaded Template :)", // Text that is to be shown in the toast
+    heading: 'Template Donwloaded', // Optional heading to be shown on the toast
+    icon: 'success', // Type of toast icon
+    showHideTransition: 'slide', // fade, slide or plain
+    allowToastClose: true, // Boolean value true or false
+    hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+    stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+    position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+    
+    
+    
+    textAlign: 'left',  // Text alignment i.e. left, right or center
+    loader: true,  // Whether to show loader or not. True by default
+    loaderBg: '#9EC600',  // Background color of the toast loader
+    beforeShow: function () {}, // will be triggered before the toast is shown
+    afterShown: function () {}, // will be triggered after the toat has been shown
+    beforeHide: function () {}, // will be triggered before the toast gets hidden
+    afterHidden: function () {}  // will be triggered after the toast has been hidden
+  });
+}
+
+const downloadFailed = () => {
+  $.toast({
+    text: "Failed to download template", // Text that is to be shown in the toast
+    heading: 'Template Download Failed', // Optional heading to be shown on the toast
+    icon: 'error', // Type of toast icon
+    showHideTransition: 'slide', // fade, slide or plain
+    allowToastClose: true, // Boolean value true or false
+    hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+    stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+    position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+    
+    
+    
+    textAlign: 'left',  // Text alignment i.e. left, right or center
+    loader: true,  // Whether to show loader or not. True by default
+    loaderBg: '#9EC600',  // Background color of the toast loader
+    beforeShow: function () {}, // will be triggered before the toast is shown
+    afterShown: function () {}, // will be triggered after the toat has been shown
+    beforeHide: function () {}, // will be triggered before the toast gets hidden
+    afterHidden: function () {}  // will be triggered after the toast has been hidden
+});
 }
 
 const updateModsString = (goonElem, valueSelected, active) => {
